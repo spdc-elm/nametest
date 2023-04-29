1a49a53c45f9ca79e1b505650b0890e045152b19
spdc-elm <58126622+spdc-elm@users.noreply.github.com>
2023-04-29 16:52:25 +0800

    Update openbox.js

--- beta/assets/openbox.js
+++ beta/assets/openbox.js
@@ -1,7 +1,7 @@
 const testMode = ['!test!\n\n', '!test!\n!\n\n'];
 let nameTesting;
 let callback;
-let cnt = 0;
+//let cnt = 0;
 let mdd;
 let id = -1;
 /**
@@ -16,10 +16,10 @@ function tenPercent(score) {
  * @param {Number} score score.
  */
 function hundredPercent(score) {
-  if (++cnt == 1) {
+  /*if (++cnt == 1) {
     return;
   }
-  cnt = 0;
+  cnt = 0;*/
   window.parent.postMessage([nameTesting, score, mdd, id], '*');
 }
 /**
@@ -45,61 +45,67 @@ function setMode(mode, i) {
  * */
 let flag = 0;
 function check() {
-  
-  if (cw().document.querySelectorAll('span.u').length <= 10) {
-    setTimeout(() => {
-      check();
-    }, 1000);
-    return;
-  }
-  const progress = cw().document.querySelectorAll('span.u');
-  let pos1 = -1;
-  let pos2 = -1;
-  for (let i = 9; i < progress.length; i++) {
-    const element = progress[i];
-    if (element.textContent.split(' ')[0] === '》') {
-      pos1 = i;
-      break;
+  if (mdd==0){
+    if (cw().document.querySelectorAll('span.u').length <= 10) {
+      setTimeout(() => {
+        check();
+      }, 1000);
+        return;
+     }
+    const progress = cw().document.querySelectorAll('span.u');
+    let pos1 = -1;
+    
+    for (let i = 9; i < progress.length; i++) {
+      const element = progress[i];
+      if (element.textContent.split(' ')[0] === '》') {
+        pos1 = i;
+        break;
+      }
     }
-  }
-  for (let i = 99; i < progress.length; i++) {
-    const element = progress[i];
-    if (element.textContent.split(' ')[0] === '》') {
-      pos2 = i;
-      break;
+    
+    if (pos1 == -1) {
+      /*10%没出*/
+      setTimeout(() => {
+        check();
+      }, 1000);
+      return;
     }
-  }
 
-  if (pos1 == -1) {
-    flag=0;/*10%没出*/
-    setTimeout(() => {
-      check();
-    }, 1000);
-    return;
+      const val = parseInt(progress[pos1].textContent.split(' ')[2]);
+      window.postMessage(val,'*');
+      console.log('sent 10% msg'+val)
+      return;
   }
   
-  if (flag==0 && pos2 == -1){
-    flag=1;/*10%已经出来了的标志*/
-    const val = parseInt(progress[pos1].textContent.split(' ')[2]);
-    window.postMessage(val,'*');
-    console.log('sent 10% msg'+val)
-    setTimeout(() => {
-      check();
-    }, 30000);
-    return;
-  } 
-  if (flag==1 && pos2 == -1) {
-    setTimeout(() => {
-      check();
-    }, 3000);
-    return;
-  }
-  if (flag==1 && pos2 != -1) {
-    const val = parseInt(progress[pos2].textContent.split(' ')[2]);
-    window.postMessage(val,'*');
-    console.log('sent 100% msg'+val)
-    flag==0;
-    return;
+  if (mdd==1){
+    if (cw().document.querySelectorAll('span.u').length <= 100) {
+      setTimeout(() => {
+        check();
+      }, 1000);
+        return;
+     }
+    
+     for (let i = 99; i < progress.length; i++) {
+      const element = progress[i];
+      if (element.textContent.split(' ')[0] === '》') {
+        pos2 = i;
+        break;
+      }
+    }
+    
+    if (pos2 == -1) {
+      /*10%没出*/
+      setTimeout(() => {
+        check();
+      }, 1000);
+      return;
+    }
+
+      const val = parseInt(progress[pos2].textContent.split(' ')[2]);
+      window.postMessage(val,'*');
+      console.log('sent 100% msg'+val)
+      return;
+    
   }
 }
 
@@ -109,7 +115,7 @@ function check() {
  * @param {Number} mode test mode.
  */
 function reload(name, mode) {
-  cnt = 0;
+  //cnt = 0;
   nameTesting = name;
   mdd = mode;
   $('#textdiv>textarea')[0].value = (mode > 1 ? testMode[1] : testMode[0]) +
@@ -118,6 +124,5 @@ function reload(name, mode) {
     $('#textdiv>textarea')[0].value += '\n' + name;
   }
   $('.goBtn')[0].click();
-  flag=0;
   check();
 }
